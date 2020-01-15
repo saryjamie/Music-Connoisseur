1cc0cb78f3835815e922223086f364b50bb323e7
Louis Alonzo <louis.a@live.com>
2020-01-14 19:20:04 -0800

    changes were made to the schema, seed file was modified accordingly to reflect the name changes made on schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
-INSERT INTO genreID (genreName, genreDecription)
+INSERT INTO genre (name, description)
 VALUES ("rock", "A style which drew heavily from the genres of blues, rhythm and blues, and from country music.");
 
-INSERT INTO genreName (genreNameID, commenterName, commentText, mediaLink)
-VALUES ("rock", "Captain Falcon", "My favorite kind of music!", "https://www.youtube.com/results?search_query=rock");
+INSERT INTO comment (commenterName, text, mediaLink)
+VALUES (Captain Falcon", "My favorite kind of music!", "https://www.youtube.com/results?search_query=rock");
 
-SELECT * FROM genreID;
-SELECT * FROM genreName;
+SELECT * FROM genre;
+SELECT * FROM comment;
